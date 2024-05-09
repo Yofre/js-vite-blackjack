fbc71ae7725283073e9efc2832ed6c6678bfed54
Yofre Garces <yofre@hotmail.com>
2024-05-09 06:12:00 -0500

    primer commit

---
+++ src/blackjack/usecases/crear-deck.js
@@ -0,0 +1,35 @@
+import _ from 'underscore';
+
+/**
+ * Esta funcion crea un nueco deck
+ * @param {Array<string>} tiposDeCarta ejemplo: ['C','D','H','S']
+ * @param {Array<string>} tiposEspeciales ejemplo: ['A','J','Q','K']
+ * @returns {Array<string>} retorna un nuevo deck de cartas
+ */
+export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+    
+    if(!tiposDeCarta || tiposDeCarta.length === 0 ) 
+    throw new Error('tipoDeCarta es obligatorio como un arreglo de string' );
+    
+    if(!tiposEspeciales || tiposEspeciales.length === 0 ) 
+    throw new Error('tiposEspeciales es obligatorio como un arreglo de string' );
+
+    let deck = [];
+
+    for( let i = 2; i <= 10; i++ ) {
+        for( let tipo of tiposDeCarta ) {
+            deck.push( i + tipo);
+        }
+    }
+
+    for( let tipo of tiposDeCarta ) {
+        for( let esp of tiposEspeciales ) {
+            deck.push( esp + tipo);
+        }
+    }
+    // console.log( deck );
+    deck = _.shuffle( deck );
+    return deck;
+}
+
+//export default crearDeck
